5f44d616166d76d1d0560fd455d253e9279bb090
Szekér Anna <anna@debian10.t.hu>
2021-02-20 14:46:40 +0200

    Fixed Django in another way

--- src/kbang_server/characterdjango.cpp
+++ src/kbang_server/characterdjango.cpp
@@ -17,19 +17,15 @@ void CharacterDjango::useAbility(QList<PlayingCard*> cards)
     if (cards.empty()) throw BadUsageException();
     if (cards.size() == 1) throw BadUsageException();
     PlayingCard* targetCard = cards.at(0);
-    if (targetCard == 0) throw BadUsageException();
     if (targetCard->owner() != mp_player) throw BadUsageException();
     if (targetCard->pocket() != POCKET_HAND) throw BadUsageException();
     PlayingCard* targetCard2 = cards.at(1);
-    if (targetCard2 == 0) throw BadUsageException();
     if (targetCard2->owner() != mp_player) throw BadUsageException();
     if (targetCard2->pocket() != POCKET_HAND) throw BadUsageException();
      mp_gatling = new CardMultiShoot(mp_player->game(), -1, CardMultiShoot::Gatling, SUIT_INVALID, 5);
-     mp_gatling->setVirtual(mp_player, POCKET_HAND);
-    //Why cann't he fire the gatling?
+     mp_gatling->setVirtual(targetCard);
      mp_gatling->play();
-    gameTable().cancelCard(targetCard);
-    gameTable().cancelCard(targetCard2);
+     gameTable().cancelCard(targetCard2);
     m_canUseAbility = false;
     notifyAbilityUse();
      
